d3e1928b8a4539cfec9300443b9483915a63a8e8
Juan Carlos Quiroz <kronogoroth@gmail.com>
2022-08-12 10:05:06 -0600

    Initial Commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { FluentProvider, teamsDarkTheme } from '@fluentui/react-components';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { getInitialResponsiveMode, initializeIcons, mergeStyles } from '@fluentui/react';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+
+
+initializeIcons();
+getInitialResponsiveMode();
+mergeStyles({
+  ':global(body,html,#root)': {
+    margin: 0,
+    padding: 0,
+    height: '100vh',
+  },
+});
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+   <React.StrictMode>
+   <FluentProvider theme={teamsDarkTheme}>
+     <App />
+   </FluentProvider>
+   </React.StrictMode>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
